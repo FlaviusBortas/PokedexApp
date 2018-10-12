248c9a352c83ef4dbedebed9fc39d00b6ec631ac
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-12 13:05:18 -0400

    Added NetworkRouter Obj

--- Pokedex/Pokedex/Networking/Service/NetworkRouter.swift
+++ Pokedex/Pokedex/Networking/Service/NetworkRouter.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> ()
+
+protocol NetworkRouter: AnyObject {
+    associatedtype EndPoint: EndPointType
+    
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
+}
+
+class Router<EndPoint: EndPointType>: NetworkRouter {
+    private var task: URLSessionTask?
+    
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion) {
+        let session = URLSession.shared
+        
+        do {
+            let request = try self.buildRequest(from: route)
+            task = session.dataTask(with: request, completionHandler: {data, response, error in
+                completion(data, response, error)
+            })
+        } catch {
+            completion(nil, nil, error)
+        }
+        self.task?.resume()
+    }
+    
+    func cancel() {
+        self.task?.cancel()
+    }
+    
+    private func buildRequest(from route: EndPoint) throws -> URLRequest {
+        
+        var request = URLRequest(url: route.baseURL.appendingPathComponent(route.path), cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
+        
+        request.httpMethod = route.httpMethod.rawValue
+        
+        do {
+            switch route.task {
+            case .request:
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+            return request
+        } catch {
+            throw error
+        }
+    }
+    
+    private func configureParameters(bodyParameters: Parameters?,
+                                         urlParameters: Parameters?,
+                                         request: inout URLRequest) throws {
+        do {
+            if let bodyParameters = bodyParameters {
+                try JSONParameterEncoder.encode(urlRequest: &request, with: bodyParameters)
+            }
+            if let urlParameters = urlParameters {
+                try JSONParameterEncoder.encode(urlRequest: &request, with: urlParameters)
+            }
+        } catch {
+            throw error
+        }
+    }
+    
+    private func addAdditionalHeaders(_ additionalHeaders: HTTPHeaders?, request: inout URLRequest) {
+        guard let headers = additionalHeaders else { return }
+        for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+    }
+}
