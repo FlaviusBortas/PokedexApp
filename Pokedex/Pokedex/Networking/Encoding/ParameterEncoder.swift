75232a00cabed5965a68364a640e40408e4dde41
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-12 13:08:56 -0400

    Added ParameterEncoder to Network Layer

--- Pokedex/Pokedex/Networking/Encoding/ParameterEncoder.swift
+++ Pokedex/Pokedex/Networking/Encoding/ParameterEncoder.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+typealias Parameters = [String: Any]
+
+protocol ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+}
+
+enum NetworkError: String, Error {
+    case parametersNil = "Parameters were nil."
+    case encodingFailed = "Parameter encoding failed."
+    case missingURL = "URL is nil."
+}
