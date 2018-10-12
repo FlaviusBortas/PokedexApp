37a84f1be17916a3efa02d1ae490b19fcd3d784b
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-12 13:08:01 -0400

    Added JSONParameterEncoder

--- Pokedex/Pokedex/Networking/Encoding/JSONParameterEncoder.swift
+++ Pokedex/Pokedex/Networking/Encoding/JSONParameterEncoder.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct JSONParameterEncoder: ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            urlRequest.httpBody = jsonData
+            
+            // What is "Content-Type"? - The Media type of the body of the request (used with POST and PUT requests).
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        } catch {
+            throw NetworkError.encodingFailed
+        }
+    }
+}
